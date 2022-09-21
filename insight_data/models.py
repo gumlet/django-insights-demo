ef3a9fc7f75a9f4bba39596ed8105727ce7f8b28
Bruce Wayne <muditsjain@gmail.com>
2022-09-21 18:45:39 +0530

    Added more fields to data

--- insight_data/models.py
+++ insight_data/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=50)
+    playback_id = models.CharField(max_length=100, default="")
+    playback_time_instant = models.FloatField(default=0)
+    video_url = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- insight_data/serializers.py
+++ insight_data/serializers.py
@@ -4,6 +4,9 @@ from .models import Event
 class EventSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(required=True, allow_blank=False, max_length=50)
+    video_url = serializers.CharField(required=True, allow_blank=False, max_length=10000)
+    playback_id = serializers.CharField(required=True, allow_blank=False, max_length=100)
+    playback_time_instant = serializers.FloatField()
 
     def create(self, validated_data):
         return Event.objects.create(**validated_data)
